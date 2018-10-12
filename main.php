f5e06124b0ac8cd8257bccb4cc8f06d7d6614595
vfe_moili <dieuwer.greevenbroek@gmail.com>
2018-10-12 17:52:36 +0200

    stuff

--- main.php
+++ main.php
@@ -1,17 +1,17 @@
 <?php
 $output = "
 <div class='project' style='float:left; display: inline-block; padding: 10px'>
-<div class='image' style='float:left; display: inline-block; margin: 5px'><img src='images/Paperless.png'></div>
+<div class='image' style='float:left; display: inline-block; margin: 5px; padding: 10px;'><div style='width: 200px; height: 200px; background: #f5fcd9;'></div></div>
 <h4>Paperless</h4>
 The assignment was simple: create a database that automatically collects and processes documents before storing them in a searchable database. Paperless Office collects documents sent as attachments to its private email address
 </div>
 <div class='project' style='float:left; display: inline-block; padding: 10px'>
-<div class='image' style='float:right; display: inline-block; margin: 5px'><img src='images/Paperless.png'></div>
+<div class='image' style='float:right; display: inline-block; margin: 5px; padding: 10px;'><div style='width: 200px; height: 200px; background: #FFA07A;'></div></div> <!-- 87CEFA-->
 <h4>QuickScan</h4>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida et tellus quis pretium. Phasellus lobortis, lectus non placerat fringilla, orci dui imperdiet leo, eget condimentum turpis augue quis mauris. Proin vestibulum, ex ut hendrerit lacinia, ipsum eros faucibus est, ac cursus neque sapien vitae turpis. Morbi semper tristique gravida. Curabitur gravida ac ex a facilisis. Nullam vitae tincidunt ligula, interdum consectetur ligula. Praesent dictum lacus massa, in convallis dui facilisis non. Aenean tempus risus id nisi maximus, ac fringilla sapien mollis. Donec pellentesque commodo nibh. Donec mi magna, tincidunt sed neque et, commodo pharetra velit. Nullam finibus ante massa, vitae maximus dui tincidunt eu. Vivamus vel orci feugiat metus tristique tempor.
 </div>
 <div class='project' style='float:left; display: inline-block; padding: 10px'>
-<div class='image' style='float:left; display: inline-block; margin: 5px'><img src='images/Paperless.png'></div>
+<div class='image' style='float:left; display: inline-block; margin: 5px; padding: 10px;'><div style='width: 200px; height: 200px; background: #E6E6FA;'></div></div> <!-- FFB6C1-->
 <h4>CASCADE</h4>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida et tellus quis pretium. Phasellus lobortis, lectus non placerat fringilla, orci dui imperdiet leo, eget condimentum turpis augue quis mauris. Proin vestibulum, ex ut hendrerit lacinia, ipsum eros faucibus est, ac cursus neque sapien vitae turpis. Morbi semper tristique gravida. Curabitur gravida ac ex a facilisis. Nullam vitae tincidunt ligula, interdum consectetur ligula. Praesent dictum lacus massa, in convallis dui facilisis non. Aenean tempus risus id nisi maximus, ac fringilla sapien mollis. Donec pellentesque commodo nibh. Donec mi magna, tincidunt sed neque et, commodo pharetra velit. Nullam finibus ante massa, vitae maximus dui tincidunt eu. Vivamus vel orci feugiat metus tristique tempor.
 </div>
